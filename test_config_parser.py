41c9cc96e6433ad2def2a291e589812fcdec2f37
Guanglin Du <guanglin.du@gmail.com>
2017-11-17 11:09:50 +0800

    Implement mail sending

--- test_config_parser.py
+++ test_config_parser.py
@@ -16,41 +16,46 @@ __version__ = '20171115a'
 # Class to read the configuration info.
 class WANTestConfigParser(object):
     def __init__(self):
-        """Read all the configuration info from file gdeployer.ini."""
+        """Read all the configuration info from file test_config.ini."""
         self.config = ConfigParser.RawConfigParser()
         self.config.read('wan_test.ini')
+        #self.config.read('./tmp/wan_test.ini')
 
-        # appids,  goagent server src dir, Python interpretor & location of GAE appcfg.py
         self.ip_addresses = self.config.get('ping', 'ip_addresses')    
-
-        # appids,  goagent server src dir, Python interpretor & location of GAE appcfg.py
         self.domain_names = self.config.get('nslookup', 'domain_names')
-        
-        # Email address & 2-step authentication
-        self.outbox = self.config.get('email', 'outbox')
-        self.inbox = self.config.get('email', 'inbox')
-        
-        # appids,  goagent server src dir, Python interpretor & location of GAE appcfg.py
         self.portals = self.config.get('portal', 'portals')
-
-        # appids,  goagent server src dir, Python interpretor & location of GAE appcfg.py
         self.web_urls = self.config.get('signin', 'web_urls')
         self.usernames = self.config.get('signin', 'usernames')        
         self.passwords = self.config.get('signin', 'passwords')
-
+        
+        # Email sending and receiving info
+        self.mail_host = self.config.get('email', 'mail_host')
+        self.mail_user = self.config.get('email', 'mail_user')
+        self.mail_pass = self.config.get('email', 'mail_pass')
+        self.mail_postfix = self.config.get('email', 'mail_postfix')
+        self.mail_to_list = self.config.get('email', 'mail_to_list')
+        self.behind_proxy = self.config.get('email', 'behind_proxy')
+        self.proxy_host = self.config.get('email', 'proxy_host')
+        self.proxy_port = self.config.get('email', 'proxy_port')
+                        
     def show_info(self):
         """Display the configuration info to debug."""
-        # [gae] section        
         print("ip_addresses = %s" % self.ip_addresses)
         print("domain_names = %s" % self.domain_names)
-        print("outbox: %s" % self.outbox)
-        print("inbox: %s" % self.inbox)
         print("portals: %s" % self.portals)
         print("web_urls: %s" % self.web_urls)
         print("usernames: %s" % self.usernames)
         print("passwords: %s" % self.passwords)
+        print("mail_host: %s" % self.mail_host)
+        print("mail_user: %s" % self.mail_user)
+        print("mail_pass: %s" % self.mail_pass)
+        print("mail_postfix: %s" % self.mail_postfix)
+        print("mail_to_list: %s" % self.mail_to_list)
+        print("behind_proxy: %s" % self.behind_proxy)
+        print("proxy_host: %s" % self.proxy_host)
+        print("proxy_port: %s" % self.proxy_port)
+        
 
 if __name__ == '__main__':
-    gdcp = WANTestConfigParser()
-    gdcp.show_info()
-    #print("--- Work done!")  
+    wtcp = WANTestConfigParser()
+    wtcp.show_info()
--- wan_test_main.py
+++ wan_test_main.py
@@ -12,6 +12,7 @@ import subprocess
 
 import ping
 import test_url
+import send_email
 import test_config_parser
 
 # A global variable to hold the test configuration.
@@ -38,8 +39,15 @@ def test_portals():
 
 
 def test_email():
-    """ Only sends an email. Please check the inbox manually."""
-    pass
+    """ Only sends an email. Please check your inbox manually."""
+    mail_to_list = [tcp.mail_to_list] # converts to an array
+    nowStr = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    news = "Hello, World!\n--- Automatically sent from the WAN test suite."
+    if send_email.send(tcp.mail_host, tcp.mail_user, tcp.mail_pass,
+                                    tcp.mail_postfix, mail_to_list, nowStr, news): 
+        print(now + " Mail sent successfully")
+    else: 
+	    print(now + " Failed sending the email!!! Please check your settings.")
 
 
 if __name__ == "__main__":
